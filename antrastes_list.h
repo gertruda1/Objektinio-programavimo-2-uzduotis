01952ddd809205559d3b0b359a6ba3588132ac83
gertruda1 <57493215+gertruda1@users.noreply.github.com>
2020-04-05 20:33:32 +0300

    Add files via upload

--- antrastes_list.h
+++ antrastes_list.h
@@ -29,9 +29,14 @@ void failugeneravimas100000 (int a);
 void failugeneravimas1000000 (int a);
 void failugeneravimas10000000 (int a);
 void skaitymas_is_failo_listas (std::list<student> &studentas, std::string kas);
-void pirmine_apklausa (std::string &kas, std::string &ar_generuoti, std::string &duomenu_ivedimas, int &koki_faila_generuoti, int &kiek_nd);
+void pirmine_apklausa (std::string &kas, std::string &ar_generuoti, std::string &duomenu_ivedimas, 
+int &koki_faila_generuoti, int &kiek_nd, int &strategija);
+void paskirstymas_listas (std::list<student> &a);
+void paskirstymas1_listas (std::list<student> &a, std::list<student> &vargs, std::list<student> &kiet);
+void paskirstymas2_listas (std::list<student> &a, std::list<student> &vargs);
 void F_duomenu_ivedimas_listas(std::string kas, std::list<student> &studentas);
 void spausdinimas_listas(std::list<student> a, std::string b);
+void spausdinimas_listas (std::list<student> kiet, std::list<student> vargs, std::string b);
 void koks_konteineris (char& simbolis, char pirmas, char antras, char trecias);
 void listas();
 
